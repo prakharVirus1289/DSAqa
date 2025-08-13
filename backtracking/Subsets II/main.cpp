62ebf6fb430d62fcf0bc4194c6bfdcd885a3b539
Prakhar2003 <131983619+prakharVirus1289@users.noreply.github.com>
2025-08-13 15:20:36 +0530

    Create main.cpp

---
+++ backtracking/Subsets II/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> res;
+        sort(nums.begin(), nums.end());
+        f(0, nums, res);
+        return big;
+    }
+private:
+    vector<vector<int>> big;
+    void f(int index, vector<int>& nums, vector<int>& res) {
+        if (index == nums.size()) {
+            big.push_back(res);
+            return;
+        }
+
+        res.push_back(nums[index]);
+        f(index+1, nums, res);
+        res.pop_back();
+
+        while (index + 1 < nums.size() && nums[index] == nums[index + 1]) {
+            index++;
+        }
+
+        f(index+1, nums, res);
+    }
+};
